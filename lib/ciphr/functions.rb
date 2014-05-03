c7a6263c8d96a70383a7f8ec9b93c8a2216ca5ba
Chris Frohoff <cfrohoff@qualcomm.com>
2014-05-03 11:04:40 -0700

    ruby rc4 impl

--- lib/ciphr/functions.rb
+++ lib/ciphr/functions.rb
@@ -756,5 +756,55 @@ module Ciphr
         @name = name
       end
     end
+
+
+    class RC4Cipher < InvertibleFunction
+      def apply
+        input, key = @args
+        keybytes = key.read.unpack('c*')
+        s = (0..255).to_a
+        j = 0
+        (0..255).each do |i|
+          j = (j + s[i] + keybytes[i % keybytes.size]) % 256
+          swp = s[i]
+          s[i] = s[j]
+          s[j] = swp
+        end
+        i = 0
+        j = 0
+
+        $stderr.puts("key: #{keybytes.inspect}")  
+
+        Proc.new do
+          byte = input.read(1)
+          $stderr.puts("byte #{byte.inspect}")
+          if byte
+            $stderr.puts("i #{i}")
+            $stderr.puts("j #{j}")
+            $stderr.puts("got input #{byte.inspect}")
+            i = (i + 1) % 256
+            j = (j + s[i]) % 256
+            swp = s[i]
+            s[i] = s[j]
+            s[j] = swp
+            k = s[(s[i] + s[j]) % 256]
+            m = [(byte.unpack('c*')[0] ^ k)].pack('c*') 
+            $stderr.puts("sending output #{m.inspect}")
+            m
+          else
+            $stderr.puts("done")
+            nil
+          end
+        end
+      end
+
+      def self.variants
+        [[['rc4-ruby'],{}]]
+      end
+
+      def self.params 
+        [:input, :key]
+      end
+    end
   end
 end
